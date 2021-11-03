b8ba917e21452b3c8cabe76a89bb7e6c0e637683
horoid <59495159+horoidrom@users.noreply.github.com>
2021-11-03 16:10:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
-free
-df -h
+mkdir cr
+cd c4
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 10.0 -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/horoid/sdp439_common_samsung.git device/samsung/sdm439-common
+git clone https://github.com/horoid/device_samsung_m01q.git device/samsung/m01q
+lunch lineage_m01q-userdebug
+mka bacon -j16
+cd out/target/product/m01q
